216180a634403802066f124b930aba810f4f45dd
Gildeir <gildeirlopes@gmail.com>
2021-05-11 23:10:33 -0300

    Adicionando 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -4,15 +4,37 @@ import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.state = {
+      movie: {},
+      loading: true,
+    };
+    this.fetchMovie = this.fetchMovie.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  async fetchMovie() {
+    const result = await movieAPI.getMovie();
+
+    this.setState({ movie: result, loading: false });
+  }
+
+  render() {
+    const { movie, loading } = this.state;
+    // Change the condition to check the state
+    if (loading) return <Loading />;
+
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
